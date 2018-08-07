b919a087ce2a742680b469d35e19645cae25de7d
Jakub Zárybnický <jakub.zarybnicky@inuits.eu>
2018-08-07 12:28:59 +0200

    Fix shell.nix

--- shell.nix
+++ shell.nix
@@ -14,16 +14,6 @@ livereload = buildPythonPackage rec {
   propagatedBuildInputs = [ markdown six tornado ];
 };
 
-markdown_include = buildPythonPackage rec {
-  pname = "markdown-include";
-  version = "0.5.1";
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "03r521ysbjjmxps8ar0q6scr19azbk4kp2akhw49lj49nmhm993j";
-  };
-  propagatedBuildInputs = [ markdown ];
-};
-
 markdown = buildPythonPackage rec {
   pname = "Markdown";
   version = "2.6.11";
@@ -34,25 +24,14 @@ markdown = buildPythonPackage rec {
   checkInputs = [ nose pyyaml pytest ];
 };
 
-pymdown-extensions = buildPythonPackage rec {
-  pname = "pymdown-extensions";
-  version = "4.9.2";
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "0a9p46il20vn95xc9qsj6q85ph64zfyqw3rg0r68dkicxmdmr4f9";
-  };
-  checkInputs = [ pyyaml pytest ];
-  propagatedBuildInputs = [ markdown ];
-};
-
 mkdocs = buildPythonApplication rec {
   pname = "mkdocs";
-  version = "0.17.3";
+  version = "1.0";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1faga4arlaq5y883zgpfgmf4iqfszmk50dycrkx7gc6y3a3rnvhj";
+    sha256 = "00m8yx14plclq9yqdm7qh3v38ichma17ly33vis2xjnn9hbd4cgp";
   };
-  propagatedBuildInputs = [ tornado livereload click pyyaml markdown_include jinja2 ];
+  propagatedBuildInputs = [ tornado livereload click pyyaml jinja2 ];
 };
 
 in
